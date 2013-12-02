f525fafd56650213eec878bb2b22d5aecd28814a
Jose Fernando <jf.junior18@hotmail.com>
2013-12-02 11:35:25 -0200

    Declaração de variavel string para teste

--- main.asm
+++ main.asm
@@ -7,6 +7,7 @@ TITLE MASM Template						(main.asm)
 INCLUDE Irvine32.inc
 .data
 myMessage BYTE "Lab2 ok-- Programa Exemplo",0dh,0ah,0
+myMessage BYTE "Linha qualquer pro Renato ver",0dh, 0ah,0
 
 .code
 main PROC
